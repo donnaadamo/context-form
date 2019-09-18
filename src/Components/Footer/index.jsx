c0b43f12acefaadc3d5266fcb5576477fc4618f3
Donna Stefania Adamo <donnaa@lagash.com>
2019-09-18 14:21:48 -0300

    config footer

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import "./styles.css";
+import { GLOBAL } from '../../Config/global';
 
 const Footer = () => {
     return (
         <Row className="header">
             <Col md={12} >
-                PORTAL | PRODUCCION | V1
+                {GLOBAL.FOOTER.DATA}
             </Col>
         </Row>
     )
